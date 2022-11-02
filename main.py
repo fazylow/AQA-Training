0e43e2cc491997ca793f75632166ca6457c8e440
rus_t <rus_tavi@yahoo.com>
2022-11-02 19:16:34 +0200

    training

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ def fizzbuzz(number):
         return "Fizz"
     else:
         return number
+
+
+def extra_case(x):
+    storage = {"FizzBuzz": 0, "Fizz": 0, "Buzz": 0}
+    for numbers in x:
+        z = fizzbuzz(numbers)
+        if z == "FizzBuzz":
+            storage["FizzBuzz"] += 1
+        if z == "Fizz":
+            storage["Fizz"] += 1
+        if z == "Buzz":
+            storage["Buzz"] += 1
+    return storage
+
+
+
--- tests/pytest.py
+++ tests/pytest.py
@@ -1,19 +1,17 @@
 from parameterized import parameterized
 from main import fizzbuzz
+from main import extra_case
 
 
 @parameterized.expand([
-    (3, "Fizz"),  # testing fizz case with 3 examples
+    (3, "Fizz"),  # testing fizz case with 2 examples
     (6, "Fizz"),
-    (9, "Fizz"),
 
-    (5, "Buzz"),  # testing buzz case with 3 examples
+    (5, "Buzz"),  # testing buzz case with 2 examples
     (-5, "Buzz"),
-    (20, "Buzz"),
 
-    (15, "FizzBuzz"),  # testing fizzbuzz case with 3 examples
+    (15, "FizzBuzz"),  # testing fizzbuzz case with 2 examples
     (0, "FizzBuzz"),
-    (30, "FizzBuzz"),
 
     (2, "2"),  # testing the case in which the number is not divided by either 3 or 5
     (4, "4"),
@@ -23,3 +21,20 @@ def test_fizzbuzz(number, expected_result):
     result = fizzbuzz(number)
     print(f" Testing number {number}, the expected result is {expected_result} and the result is {result}")
     assert str(result) == expected_result
+
+
+@parameterized.expand([
+    ([3, 5, 15, 20, 25], {"FizzBuzz": 1, "Fizz": 1, "Buzz": 3}),
+
+
+    ([3, 9, 6], {"FizzBuzz": 0, "Fizz": 3, "Buzz": 0}),
+
+
+    ([15, 30], {"FizzBuzz": 2, "Fizz": 0, "Buzz": 0}),
+
+
+])
+def test_extra_case(x, expected_result):
+    result = extra_case(x)
+    print(f" Testing outcome for {x}, the expected result is {expected_result} and the result is {result}")
+    assert result == expected_result
